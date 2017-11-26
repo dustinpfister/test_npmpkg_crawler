04b5a4453ba46c7271fbc35e0b3401361af1f02c
dustinpfister <dustin.pfister@gmail.com>
2017-11-26 18:45:49 -0500

    some new demos

---
+++ basic.js
@@ -0,0 +1,31 @@
+// basic crawler useage
+
+var Crawler = require('crawler');
+
+// create a new crawler
+var c = new Crawler({
+
+        maxConnections: 1,
+        jQuery: false,
+
+        // This will be called for each crawled page
+        callback: function (err, res, done) {
+
+            if (err) {
+
+                console.log(err);
+
+            } else {
+
+                // the body of the page
+                console.log(res.body);
+
+            }
+
+            done();
+        }
+
+    });
+
+// query a single url
+c.queue('http://www.google.com');
