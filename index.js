b4eee32bcefb3b00706ced74949269646c5580d4
dustinpfister <dustin.pfister@gmail.com>
2017-11-26 12:02:50 -0500

    forget hapi, okay come on work

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
+
+var http = require("http");
+
+var server = http.createServer(function (req, res) {
+
+        res.writeHead(500, {
+            "Content-Type": "text/plain"
+        });
+        res.write('oh look it works, I guess openshift doesnt suck after all');
+        res.end();
+
+    });
+
+server.listen(8080,'172.30.231.52');
+
+/*
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
+ */
 
 // does not work
 // 'nodejs-crawl-test1-name-13-26rlm'
 // '172.30.231.52'
 
+/*
 // log env here before it fails
 console.log('process.env:');
 console.log(process.env);
 
 server.connection({
-    port: 8080,
-    host: 'tcp://172.30.231.52' //openShift.host || 'localhost'
+port: 8080,
+host: '172.30.231.52' //openShift.host || 'localhost'
 });
 
 server.register(require('inert'), function (e) {
 
-    if (e) {
+if (e) {
 
-        console.log(e);
+console.log(e);
 
-    }
+}
 
-    server.route({
-        method: 'GET',
-        path: '/{param*}',
-        handler: {
-            directory: {
-                path: 'public',
-                listing: true
-            }
-        }
-    });
+server.route({
+method: 'GET',
+path: '/{param*}',
+handler: {
+directory: {
+path: 'public',
+listing: true
+}
+}
+});
 
-    server.start(function (e) {
+server.start(function (e) {
 
-        if (e) {
+if (e) {
 
-            console.log(e);
+console.log(e);
 
-        } else {
+} else {
 
-            console.log('hapi server up:');
-            console.log('port: ' + server.info.port);
-            console.log('uri: ' + server.info.uri);
-            console.log('');
-            console.log('OPENSHIFT_APP_NAME: ' + process.env['OPENSHIFT_APP_NAME']);
-            console.log('HOSTNAME: ' + process.env['HOSTNAME']);
-            console.log('process.env:');
-            console.log(process.env);
+console.log('hapi server up:');
+console.log('port: ' + server.info.port);
+console.log('uri: ' + server.info.uri);
+console.log('');
+console.log('OPENSHIFT_APP_NAME: ' + process.env['OPENSHIFT_APP_NAME']);
+console.log('HOSTNAME: ' + process.env['HOSTNAME']);
+console.log('process.env:');
+console.log(process.env);
 
-        }
-
-    });
+}
 
 });
+
+});
+*/
